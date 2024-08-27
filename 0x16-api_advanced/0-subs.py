8ee3ebb2292cbe93485c5165e330a2176da3fc31
alitalaat22 <elshaaerali16@gmail.com.com>
2024-08-27 14:25:59 +0300

    dev

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""
-number of subscribers for a given subreddit
-"""
-
-from requests import get
+'''
+    module for querying Reddit API
+'''
+import requests as req
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
+    '''
+        get total subscribers of a subreddit
+    '''
     try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
+        res = req.get('https://www.reddit.com/r/{}/about.json'
+                      .format(subreddit),
+                      headers={"user-agent": 'ames'}).json()
+        return res['data']['subscribers']
+    except KeyError:
         return 0
