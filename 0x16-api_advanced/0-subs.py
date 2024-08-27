81aa4d17cf13cc7b93d7e20eba058c1d8afe10a1
alitalaat22 <elshaaerali16@gmail.com.com>
2024-08-27 14:29:38 +0300

    dev

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-'''
-    module for querying Reddit API
-'''
-import requests as req
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-    '''
-        get total subscribers of a subreddit
-    '''
-    try:
-        res = req.get('https://www.reddit.com/r/{}/about.json'
-                      .format(subreddit),
-                      headers={"user-agent": 'ames'}).json()
-        return res['data']['subscribers']
-    except KeyError:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
